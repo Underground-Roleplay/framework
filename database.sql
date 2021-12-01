8c7b758f16b88d2d956b75205891faed9c9269c9
xipanca <xipanca@gmail.com>
2021-12-01 12:38:59 -0500

    update sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           8.0.19 - MySQL Community Server - GPL
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.0.0.5919
+-- Host:                         127.0.0.1
+-- Server version:               8.0.23 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,8 +10,14 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Copiando estrutura para tabela urp.characters
+
+-- Dumping database structure for urp
+CREATE DATABASE IF NOT EXISTS `urp` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `urp`;
+
+-- Dumping structure for table urp.characters
 CREATE TABLE IF NOT EXISTS `characters` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ssn` varchar(50) NOT NULL,
@@ -25,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `position` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci,
   `metadata` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci,
   `inventory` longtext,
+  `clothes` longtext,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ssn`),
   KEY `id` (`id`),
@@ -32,9 +39,9 @@ CREATE TABLE IF NOT EXISTS `characters` (
   KEY `socialID` (`socialID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Data exporting was unselected.
 
--- Copiando estrutura para tabela urp.characters_customs
+-- Dumping structure for table urp.characters_customs
 CREATE TABLE IF NOT EXISTS `characters_customs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ssn` varchar(255) NOT NULL,
@@ -45,9 +52,9 @@ CREATE TABLE IF NOT EXISTS `characters_customs` (
   KEY `ssn` (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Exportação de dados foi desmarcado.
+-- Data exporting was unselected.
 
--- Copiando estrutura para tabela urp.permissions
+-- Dumping structure for table urp.permissions
 CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -55,11 +62,10 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `permission` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `socialID` (`socialID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela urp.users
+-- Dumping structure for table urp.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `identifier` char(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
@@ -67,10 +73,24 @@ CREATE TABLE IF NOT EXISTS `users` (
   `whitelisted` int DEFAULT NULL,
   `socialID` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Data exporting was unselected.
+
+-- Dumping structure for table urp.vehicles
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `plate` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `status` longtext CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `metadata` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `ssn` (`ssn`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
