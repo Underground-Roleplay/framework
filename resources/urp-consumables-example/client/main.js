661baa140127524f92ef1cf2d2c5f75424150a2f
nickplayz <nickgameplayz@gmail.com>
2021-12-06 16:46:36 -0300

    feat(urp-consumables): adds armor and sandwich as example items

--- resources/urp-consumables-example/client/main.js
+++ resources/urp-consumables-example/client/main.js
@@ -2,14 +2,25 @@ import * as alt from 'alt-client';
 import Core from 'urp-core';
 
 alt.onServer('consumables:drink', (item) => {
-    let dict;
-    let anim;
+    const dict = 'amb@world_human_drinking@beer@male@idle_a';
+    const anim = 'idle_a'
     let prop;
     switch (item.name) {
         case 'water_bottle':
-            dict = 'amb@world_human_drinking@beer@male@idle_a'
-            anim = 'idle_a'
             prop = 'ba_prop_club_water_bottle'
+            break;
+    }
+    Core.Functions.playAnim(dict, anim, 5000, 49, { name: prop, hand: 28422 })
+    alt.setTimeout(()=>{Core.Functions.stopAnim()}, 5000)
+})
+
+alt.onServer('consumables:eat', (item) => {
+    const dict = 'amb@code_human_wander_eating_donut@male@idle_a';
+    const anim = 'idle_c';
+    let prop;
+    switch (item.name) {
+        case 'sandwich':
+            prop = 'prop_sandwich_01'
             break;
     }
     Core.Functions.playAnim(dict, anim, 5000, 49, { name: prop, hand: 28422 })
--- resources/urp-consumables-example/server/main.js
+++ resources/urp-consumables-example/server/main.js
@@ -1,3 +1,11 @@
+import * as alt from 'alt-server';
 import Core from 'urp-core';
 
 Core.Inventory.createUseableItem('water_bottle', 'consumables:drink', false)
+Core.Inventory.createUseableItem('sandwich', 'consumables:eat', false)
+
+Core.Inventory.createUseableItem('armor', 'consumables:useArmor', true)
+
+alt.on('consumables:useArmor', (source, item) => {
+    source.armour = 100
+})
