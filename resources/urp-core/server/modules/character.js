30f3c82179ea9214a5c20d466dc8bd123ecc8e69
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2021-12-29 23:06:28 -0300

    fix playerData.phone

--- resources/urp-core/server/modules/character.js
+++ resources/urp-core/server/modules/character.js
@@ -21,7 +21,7 @@ const startCharacter = async (source, newCharacter = undefined) => {
         playerData.position = JSON.parse(playerData.position)
         playerData.metadata = JSON.parse(playerData.metadata)
         playerData.charinfo = JSON.parse(playerData.charinfo)
-        playerData.phone = charinfo.phone
+        playerData.phone =  playerData.charinfo.phone
         playerData.inventory = playerData.inventory ? JSON.parse(playerData.inventory) : []
         checkPlayerData(source, playerData)
         return;
