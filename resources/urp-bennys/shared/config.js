5a68b6543b91d7dcd2d984745221bc217e4ddd49
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2021-12-30 18:24:47 -0300

    add bennys

---
+++ resources/urp-bennys/shared/config.js
@@ -0,0 +1,19 @@
+import * as alt from 'alt'
+export const CLOTH_STORES = [
+    {
+	"x": 11.6,
+	"y": 6514.2,
+    "z": 30.9,
+    "type": "Cloth Store",
+    "isServer": false,
+    "eventName": "Skinshop:open",
+      "blip":{
+         "sprite":73,
+         "color":5
+      },
+      marker:{
+        type: 1,
+        color: new alt.RGBA(0, 181, 204, 200)
+    }
+    }
+];
