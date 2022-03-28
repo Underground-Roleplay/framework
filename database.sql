56db740e04db6cb4a12870893c7041291c2cf1fb
xipanca <xipanca@gmail.com>
2022-03-28 15:35:22 -0400

    add new table(storage)

--- database.sql
+++ database.sql
@@ -1,31 +1,33 @@
--- Dumping database structure for urp
-CREATE DATABASE IF NOT EXISTS `urp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE IF NOT EXISTS `urp` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `urp`;
 
--- Dumping structure for table urp.characters
 CREATE TABLE IF NOT EXISTS `characters` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ssn` varchar(50) NOT NULL,
   `dimension` int DEFAULT NULL,
   `socialID` varchar(255) NOT NULL,
+  `metadata` mediumtext CHARACTER SET utf8 NOT NULL,
   `name` varchar(255) NOT NULL,
   `money` text NOT NULL,
   `charinfo` text NOT NULL,
   `job` mediumtext NOT NULL,
-  `gang` text DEFAULT NULL,
+  `gang` text,
   `position` mediumtext NOT NULL,
-  `metadata` mediumtext NOT NULL,
   `inventory` longtext NOT NULL,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ssn`),
   KEY `id` (`id`),
   KEY `last_updated` (`last_updated`),
   KEY `socialID` (`socialID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
--- Dumping structure for table urp.characters_customs
 CREATE TABLE IF NOT EXISTS `characters_customs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ssn` varchar(255) NOT NULL,
@@ -34,11 +36,65 @@ CREATE TABLE IF NOT EXISTS `characters_customs` (
   `cloakroom` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`ssn`) USING BTREE,
   KEY `ssn` (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+CREATE TABLE IF NOT EXISTS `characters_homes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(255) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `chest` longtext,
+  `slot` int DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `home` (`name`,`slot`) USING BTREE,
+  KEY `ssn` (`ssn`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping structure for table urp.permissions
+CREATE TABLE IF NOT EXISTS `characters_vehicles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `plate` varchar(50) NOT NULL,
+  `status` longtext NOT NULL,
+  `metadata` longtext NOT NULL,
+  `customizations` longtext NOT NULL,
+  `inventory` longtext,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `ssn` (`ssn`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_player_one` varchar(50) NOT NULL,
+  `id_player_two` varchar(50) NOT NULL,
+  `contain_message` int DEFAULT '0',
+  `last_message` varchar(500) DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `chat_message` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) DEFAULT '',
+  `message` varchar(50) NOT NULL DEFAULT '0',
+  `ssn` varchar(50) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `read` int DEFAULT '0',
+  `deleted` int DEFAULT '0',
+  `chave_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `FK_chat_message_chat` (`chave_id`) USING BTREE,
+  CONSTRAINT `FK_chat_message_chat` FOREIGN KEY (`chave_id`) REFERENCES `chat` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `dealership` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `model` varchar(50) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -46,11 +102,33 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `permission` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `socialID` (`socialID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+CREATE TABLE IF NOT EXISTS `phone_contact` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(20) NOT NULL,
+  `contact` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping structure for table urp.users
+CREATE TABLE IF NOT EXISTS `phone_recent` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(20) NOT NULL,
+  `recent` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `storages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ssn` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `data` longtext CHARACTER SET utf8 NOT NULL,
+  `name` varchar(250) CHARACTER SET utf8 NOT NULL,
+  `perm` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `last_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `ssn` (`ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `identifier` char(255) NOT NULL DEFAULT '',
@@ -58,103 +136,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `whitelisted` int DEFAULT NULL,
   `socialID` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
--- Dumping structure for table urp.characters_vehicles
-CREATE TABLE IF NOT EXISTS `characters_vehicles` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ssn` varchar(255) NOT NULL NOT NULL,
-  `model` varchar(255) NOT NULL NOT NULL,
-  `position` varchar(255) NOT NULL NOT NULL,
-  `plate` varchar(50) NOT NULL NOT NULL,
-  `status` longtext NOT NULL NOT NULL,
-  `metadata` longtext NOT NULL NOT NULL,
-  `customizations` longtext NOT NULL,
-  `inventory` longtext NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `ssn` (`ssn`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
--- Dumping structure for table urp.characters_homes
-CREATE TABLE IF NOT EXISTS `characters_homes`(
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ssn` varchar(255) NOT NULL,
-  `name` VARCHAR(100),
-  `chest` LONGTEXT NULL ,
-  `slot` INTEGER,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `home` (`name`,`slot`) USING BTREE,
-  KEY `ssn` (`ssn`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-
-CREATE TABLE `chest` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`cords` VARCHAR(50) NOT NULL DEFAULT '',
-	`chest` LONGTEXT NOT NULL ,
-	PRIMARY KEY (`id`) USING BTREE
-)
-ENGINE=InnoDB
-AUTO_INCREMENT=2
-;
--- Data exporting was unselected.
-
-CREATE TABLE IF NOT EXISTS dealership (
-  id int NOT NULL AUTO_INCREMENT,
-  model varchar(50) DEFAULT NULL,
-  category varchar(50) DEFAULT NULL,
-  stock int DEFAULT NULL,
-  price int DEFAULT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `chat` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`id_player_one` VARCHAR(50) NOT NULL,
-	`id_player_two` VARCHAR(50) NOT NULL,
-	`contain_message` INT(10) NULL DEFAULT '0',
-	`last_message` VARCHAR(500) NULL DEFAULT '',
-	PRIMARY KEY (`id`) USING BTREE
-)
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE IF NOT EXISTS `chat_message` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`type` VARCHAR(50) NULL DEFAULT '',
-	`message` VARCHAR(50) NOT NULL DEFAULT '0',
-	`ssn` VARCHAR(50) NOT NULL,
-	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`read` INT(10) NULL DEFAULT '0',
-	`deleted` INT(10) NULL DEFAULT '0',
-	`chave_id` INT(10) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `FK_chat_message_chat` (`chave_id`) USING BTREE,
-	CONSTRAINT `FK_chat_message_chat` FOREIGN KEY (`chave_id`) REFERENCES `urp`.`chat` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE IF NOT EXISTS `phone_contact` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`ssn` VARCHAR(20) NOT NULL,
-	`contact` VARCHAR(1000) NOT NULL,
-	PRIMARY KEY (`id`) USING BTREE
-)
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-CREATE TABLE IF NOT EXISTS `phone_recent` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`ssn` VARCHAR(20) NOT NULL,
-	`recent` VARCHAR(4000) NULL,
-	PRIMARY KEY (`id`) USING BTREE
-)
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
